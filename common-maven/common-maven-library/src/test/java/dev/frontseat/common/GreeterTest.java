52a95854010ebc53115b458eaebd1121f55891c5
jbadeau <jose.badeau@juliusbaer.com>
2026-02-16 11:34:40 +0100

    feat: Fix maven build

--- common-maven/common-maven-library/src/test/java/dev/frontseat/common/GreeterTest.java
+++ common-maven/common-maven-library/src/test/java/dev/frontseat/common/GreeterTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class GreeterTest {
+public class GreeterTest {
 
     @Test
     void greetReturnsExpectedMessage() {
--- maven/maven-library/src/test/java/dev/frontseat/library/FancyGreeterTest.java
+++ maven/maven-library/src/test/java/dev/frontseat/library/FancyGreeterTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class FancyGreeterTest {
+public class FancyGreeterTest {
 
     @Test
     void greetReturnsFancyMessage() {
