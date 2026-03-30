c1dc1c5358d80250e858b69b92ae6362e1063fb5
jbadeau <jose.badeau@juliusbaer.com>
2026-03-30 12:21:03 +0200

    build: replace jib with gib

--- cartservice/tests/obj/Release/net10.0/cartservice.tests.AssemblyInfo.cs
+++ cartservice/tests/obj/Release/net10.0/cartservice.tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cartservice.tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a53de0a0485ac77c19d38f46848c7f69158831a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffea9b0487244e7091b85943e733ad0876d11531")]
 [assembly: System.Reflection.AssemblyProductAttribute("cartservice.tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cartservice.tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
